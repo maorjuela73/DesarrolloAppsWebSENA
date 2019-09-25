74ace521347de44b1ba5125dfe8476ecff130319
Miguel Orjuela <ma.orjuela73@gmail.com>
2019-09-25 17:07:05 -0500

    Prsentacin de Javascript y ejemplo2

---
+++ ejemplos/ejemplo2/script1.js
@@ -0,0 +1,23 @@
+// console.log("Hola mundo desde el archivo");
+
+// var x = -15;
+// x >= 0 ? console.log(x) : console.log(-x);
+
+// {
+//   x = Math.PI;
+//   cx = Math.cos(x);
+//   console.log('cx:', cx)
+// }
+
+var text;
+var x = 10;
+
+switch(x) {
+  case 10:
+    text = "Decena";
+    break;
+  default:
+    text = "Sin valor";
+}
+
+console.log(text);
