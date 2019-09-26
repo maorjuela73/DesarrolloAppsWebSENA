ae3ecfe387839bcd087ea2130f9fae0d4b1a3af3
Miguel Orjuela <ma.orjuela73@gmail.com>
2019-09-26 14:34:38 -0500

    Ejercicio 4 finalizado

--- ejemplos/ejemplo4/script.js
+++ ejemplos/ejemplo4/script.js
@@ -1,1 +1,36 @@
-console.log("Vamos");
+// Se incluyen logo y contenedor
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.jpg';
+
+const container = document.createElement('div');
+container.setAttribute('class','container');
+console.log(container);
+
+app.appendChild(logo);
+app.appendChild(container);
+
+// Se comienzan a pedir datos
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+request.onload = function () {
+    var data = JSON.parse(this.response);
+
+    data.forEach(movie => {
+        // console.log(movie.title);
+        let titulo = document.createElement('h4');
+        titulo.setAttribute('class','card');
+        titulo.textContent = movie.title;
+        let descripcion = document.createElement('p');
+        descripcion.innerHTML = movie.description;
+        container.appendChild(titulo);
+        container.appendChild(descripcion);
+    })
+}
+
+request.send();
+
+
